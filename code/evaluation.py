d21884c6f85ba183de15b8535b7f345293bb9472
LeeAnnJ <lee_ann.j@qq.com>
2025-07-17 21:47:31 +0800

    update gitignore

--- code/evaluation.py
+++ code/evaluation.py
@@ -40,10 +40,11 @@ def run(operation):
     if operation == 'baseline':
         exract_baseline_coverage(FS, TS, BL, dataset_info)
     if operation == 'check':
-        from evaluations.check_empty_class import check_empty_class, compare_baseline
-        # check_empty_class(FS, dataset_info)
-        hits_result = "../evaluation/baseline/HITS/gpt4o"
-        compare_baseline(FS, dataset_info, hits_result)
+        import evaluations.check_empty_class as Check
+        # Check.check_empty_class(FS, dataset_info)
+        # hits_result = "../evaluation/baseline/HITS/gpt4o"
+        # Check.compare_baseline(FS, dataset_info, hits_result)
+        Check.filter_all_passed_classes(dataset_info, BL)
     return
 
 
